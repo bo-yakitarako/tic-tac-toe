863e2d5851d3d5cf545741fd94d4a9c92043a374
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2026-03-28 22:29:53 +0900

    チョロボン

--- src/main.ts
+++ src/main.ts
@@ -7,30 +7,14 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
-client.once(Events.ClientReady, () => {
-  console.log('バカにしやがってると負けんぞ');
-});
-
-// ホットリロード時の重複リスナー登録を防ぐ
-client.removeAllListeners(Events.InteractionCreate);
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (interaction.isChatInputCommand()) {
-    await slashCommandsInteraction(interaction);
-  }
-  if (interaction.isButton()) {
-    await buttonInteraction(interaction);
-  }
-  if (interaction.isStringSelectMenu()) {
-    await selectMenuInteraction(interaction);
-  }
-});
-
 const TOKEN = process.env.TOKEN as string;
 const CLIENT_ID = process.env.CLIENT_ID as string;
 const GUILD_ID = process.env.GUILD_ID ?? null;
-const rest = new REST().setToken(TOKEN);
-void (async () => {
+
+client.once(Events.ClientReady, async () => {
+  console.log('バカにしやがってると負けんぞ');
+
+  const rest = new REST().setToken(TOKEN);
   try {
     if (GUILD_ID !== null) {
       await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
@@ -44,5 +28,18 @@ void (async () => {
   } catch (error) {
     console.error(error);
   }
-})();
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (interaction.isChatInputCommand()) {
+    await slashCommandsInteraction(interaction);
+  }
+  if (interaction.isButton()) {
+    await buttonInteraction(interaction);
+  }
+  if (interaction.isStringSelectMenu()) {
+    await selectMenuInteraction(interaction);
+  }
+});
+
 void client.login(TOKEN);
